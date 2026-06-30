d4633c8b24b8dc119412db9d3af6311eabff703c
devkasun <kasun2014@outlook.com>
2026-06-30 21:36:29 +0530

    fix: routing issues on view transition api page

---
+++ src/routes/experiments/view-transition-api/index.tsx
@@ -0,0 +1,17 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { experiments } from "../../../data/experiments";
+import ExperimentLayout from "../../../components/ExperimentLayout";
+
+export const Route = createFileRoute("/experiments/view-transition-api/")({
+    component: ViewTransitions,
+});
+
+function ViewTransitions() {
+    const experiment = experiments.find((e) => e.slug === "view-transition-api")!;
+    return (
+        <ExperimentLayout
+            experiment={experiment}
+            allExperiments={experiments}
+        />
+    );
+}
